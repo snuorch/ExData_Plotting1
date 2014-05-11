d6f6f992e10c23ba3876e7f69aa703b3765719b7
Salvatore <natale.salvatore@gmail.com>
2014-05-12 01:20:38 +0200

    Plot4
    Plot4

---
+++ plot4.R
@@ -0,0 +1,45 @@
+#read csv file (change default separator ";")
+mydataset <- read.csv("inputfile//household_power_consumption.txt",  sep=";")
+
+#Convert Date column from factor to Date
+mydataset$Date <- as.Date(mydataset$Date, format = "%d/%m/%Y")
+
+#Subset DataFrame in a new DataFrame according to Course Project (date between 2007-02-01 and 2007-02-02)
+newdataset <- mydataset[mydataset$Date >= "2007-02-01" & mydataset$Date <= "2007-02-02",]
+
+newdataset$Global_active_power <- as.numeric(as.character(newdataset$Global_active_power))
+newdataset$Global_reactive_power <- as.numeric(as.character(newdataset$Global_reactive_power))
+newdataset$Voltage <- as.numeric(as.character(newdataset$Voltage))
+newdataset$Sub_metering_1 <- as.numeric(as.character(newdataset$Sub_metering_1))
+newdataset$Sub_metering_2 <- as.numeric(as.character(newdataset$Sub_metering_2))
+newdataset$Sub_metering_3 <- as.numeric(as.character(newdataset$Sub_metering_3))
+
+#create new colomn pasting Date and time
+newdataset$CompleteDate <- paste(newdataset$Date, newdataset$Time)
+newdataset$CompleteDate <- strptime(newdataset$CompleteDate, "%Y-%m-%d %H:%M:%S")
+
+par(mfrow=c(2,2))
+Sys.setlocale("LC_TIME", "en_US.UTF-8")
+
+#plot1
+plot(newdataset$CompleteDate, newdataset$Global_active_power, type = "l", xlab = "", ylab = "Global Active Power (kilowatts)")
+
+#plot2
+plot(newdataset$CompleteDate, newdataset$Voltage, type = "l", xlab = "", ylab = "Voltage")
+
+#Plot3
+plot(newdataset$CompleteDate, newdataset$Sub_metering_1, type = "l", xlab = "", ylab = "Energy Sub metering")
+lines(newdataset$CompleteDate, newdataset$Sub_metering_2, type = "l", col="red")
+lines(newdataset$CompleteDate, newdataset$Sub_metering_3, type = "l", col="blue")
+
+#plot4
+plot(newdataset$CompleteDate, newdataset$Global_active_power, type = "l")
+
+
+legend('topright', c('Sub_metering_1','Sub_metering_2','Sub_metering_3'),col=c('black','red','blue'));
+#Save Filede
+dev.copy(png, file = "plot4.png")
+
+dev.off()
+
+
