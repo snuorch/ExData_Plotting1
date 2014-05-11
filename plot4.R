ed86bd6a01d0f0199784fd6a7f08180cb091e892
Salvatore <natale.salvatore@gmail.com>
2014-05-12 01:44:56 +0200

    Revise

--- plot4.R
+++ plot4.R
@@ -31,7 +31,7 @@ plot(newdataset$CompleteDate, newdataset$Voltage, type = "l", xlab = "datetime",
 plot(newdataset$CompleteDate, newdataset$Sub_metering_1, type = "l", xlab = "", ylab = "Energy Sub metering")
 lines(newdataset$CompleteDate, newdataset$Sub_metering_2, type = "l", col="red")
 lines(newdataset$CompleteDate, newdataset$Sub_metering_3, type = "l", col="blue")
-legend('topright', c('Sub_metering_1','Sub_metering_2','Sub_metering_3'),pch=1,col=c('black','red','blue'),border=FALSE);
+legend('topright', c('Sub_metering_1','Sub_metering_2','Sub_metering_3'), lty=c(1,1,1), lwd=c(1,1,1), col=c('black','red','blue'),bty='n')
 
 #plot4
 plot(newdataset$CompleteDate, newdataset$Global_reactive_power, type = "l", xlab = "datetime", ylab = "Global_reactive_power")
