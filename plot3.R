ab001faa4f6725fe601878f398cf62cfa018153a
Salvatore <natale.salvatore@gmail.com>
2014-05-12 01:54:32 +0200

    Revise

--- plot3.R
+++ plot3.R
@@ -20,7 +20,7 @@ plot(newdataset$CompleteDate, newdataset$Sub_metering_1, type = "l", xlab = "", 
 lines(newdataset$CompleteDate, newdataset$Sub_metering_2, type = "l", col="red")
 lines(newdataset$CompleteDate, newdataset$Sub_metering_3, type = "l", col="blue")
 
-legend('topright', c('Sub_metering_1','Sub_metering_2','Sub_metering_3'), lty=c(1,1,1), lwd=c(1,1,1), col=c('black','red','blue'),bty='n')
+legend('topright', c('Sub_metering_1','Sub_metering_2','Sub_metering_3'), cex=.6, lty=c(1,1,1), lwd=c(1,1,1), col=c('black','red','blue'))
 #Save Filede
 dev.copy(png, file = "plot3.png")
 
